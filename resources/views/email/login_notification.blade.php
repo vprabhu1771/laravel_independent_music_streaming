5a48437dbd77671d938c8a859d867a67d6acf12c
Prabhu <vprabhu1771@gmail.com>
2025-02-14 09:27:11 +0530

    LoginNotification Mail added
    php artisan make:mail LoginNotification

---
+++ resources/views/email/login_notification.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Notification</title>
+</head>
+<body>
+    <h2>Hello, {{ $user->name }}</h2>
+    <p>You have successfully logged in to your account.</p>
+    <p>If this wasn't you, please reset your password immediately.</p>
+    <p>Thank you, <br> Your Company</p>
+</body>
+</html>
