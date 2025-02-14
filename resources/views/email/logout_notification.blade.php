bbcf7fc86547c550167f23b814481724f416c8e5
Prabhu <vprabhu1771@gmail.com>
2025-02-14 09:28:37 +0530

    LogoutNotification Mail added
    php artisan make:mail LogoutNotification

---
+++ resources/views/email/logout_notification.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Logout Notification</title>
+</head>
+<body>
+    <h2>Hello, {{ $user->name }}</h2>
+    <p>You have successfully logout in to your account.</p>
+    <p>If this wasn't you, please reset your password immediately.</p>
+    <p>Thank you, <br> Your Company</p>
+</body>
+</html>
